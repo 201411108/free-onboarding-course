65d9117ed111900887f6037991831080dd479e96
201411108 <aggsae@gmail.com>
2021-07-23 16:30:32 +0900

    수정: 불필요한 구문 제거

--- src/components/Desktop/DesktopNavigationMenu.tsx
+++ src/components/Desktop/DesktopNavigationMenu.tsx
@@ -1,37 +1,16 @@
-/* eslint-disable prefer-template */
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './DesktopNavigationMenu.css'
 
 function DesktopNavigationMenu() {
-    const [windowSize, setWindowSize] = useState(window.innerWidth)
-
-    const handleResize = () => {
-        setWindowSize(window.innerWidth)
-    }
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize)
-    })
-
-    const homeLi = <li className={'home ' + (windowSize >= 768 ? 'phoneOnly' : '')}>홈</li>
-
     return (
         <ul>
-            { (windowSize < 768) ?
-                homeLi : <div/>
-            }
             <li>탐색</li>
             <li>커리어 성장</li>
-            { (windowSize >= 768) ?
-                <>
-                    <li className='desktopVisible'>직군별 연봉</li>
-                    <li className='desktopVisible'>이력서</li>
-                    <li className='desktopVisible'>매치업</li>
-                    <li className='desktopVisible'>프리랜서</li>
-                    <li className='desktopVisible'>AI 합격예측</li>
-                </> : 
-                <div/>
-            }
+            <li>직군별 연봉</li>
+            <li>이력서</li>
+            <li>매치업</li>
+            <li>프리랜서</li>
+            <li>AI 합격예측</li>
         </ul>
     )
 }

7adea554815df563f0cb2b4e6eb89759b5150ec2
201411108 <aggsae@gmail.com>
2021-07-28 22:11:32 +0900

    수정: Logo 컴포넌트 수정
    Logo.tsx 및 Logo.css 내 미사용 구문 제거
    Logo.css 내에서 로고의 스타일 적용 변경

---
+++ src/components/Desktop/Logo.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prefer-template */
+import React from 'react'
+import './Logo.css'
+
+// 로고 작은 화면에서는 안보여야 한다.
+function Logo() {
+    return (
+        <div className='logo '>wanted</div>
+    )
+    
+}
+
+export default Logo

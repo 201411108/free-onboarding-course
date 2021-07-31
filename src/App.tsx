1d1ef3f4935ccf4fd7fdd7266474f51fb83eae00
201411108 <aggsae@gmail.com>
2021-07-31 23:07:23 +0900

    구현: 누락된 MediaQuery 반영 기록록

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react'
 import { Desktop, Mobile } from './MediaQuery'
 import DesktopNavigationBar from './components/Desktop/DesktopNavigationBar'

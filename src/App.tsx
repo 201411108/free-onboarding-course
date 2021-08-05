ac3a3f06e7282971ce6a820ae10158559e3a1960
201411108 <aggsae@gmail.com>
2021-08-05 22:31:38 +0900

    구현: GlobalNavigationBar 고정 구현
    페이지에 포함되는 내용에 상관없이 global navigation bar가 상단에
    고정되도록 구현 완료
    페이지에 포함되는 내용에 해당하는 content 컴포넌트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Desktop, Mobile } from './MediaQuery'
 import DesktopNavigationBar from './components/Desktop/DesktopNavigationBar'
+import Content from './components/Content'
 import './App.css'
 
 function App() {
   return (
-    <div className='App'>
-      <Desktop>
-        <DesktopNavigationBar />
-      </Desktop>
-      <Mobile><div>This is Mobile</div></Mobile>
-    </div>
+    <>
+      <div className='App'>
+        <Desktop>
+          <DesktopNavigationBar />
+        </Desktop>
+        <Mobile><div>This is Mobile</div></Mobile>
+      </div>
+      <Content />
+    </>
   )
 }
 

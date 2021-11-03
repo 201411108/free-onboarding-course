f6fe2e05d8f9c92bbd8ca60bdfa35267c20f6df6
201411108 <aggsae@gmail.com>
2021-11-03 22:38:27 +0900

    feat: add mouse over event handling using css
    1. 모바일 화면에서 홈, 탐색, 커리어 성장에 마우스 올릴 때 파란색 밑줄이
    생기는 효과 부여
        * 해결해야 하는 이슈들 TODO 주석 참고

--- src/components/GNBComponents/MenuComponent.tsx
+++ src/components/GNBComponents/MenuComponent.tsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import '../style/MenuComponent.css'
 
 function MenuComponent() {
     return (
         <ul className='menu-ul'>
-            {/* TODO :: 탐색 및 커리어 성장에 마우스 올렸을 때 파란색 밑줄이 뜨도록 */}
+            {/* TODO :: 
+                    AS-IS :: 마우스 올릴 때 파란 밑줄
+                    TO-BE :: 해당 페이지에 있을 때 활성화, 데스크톱에서는 회색 밑줄
+                             desktop, mobile이 반대로 되어 있음. 각 보이는 화면에 대해서(mobile -> mobile에서만 보이는 요소라는 뜻)
+              */}
             <li className='menu-li desktop'><Link className='menu-link' to='/search'>탐색</Link></li>
             <li className='menu-li desktop'><Link className='menu-link' to='/career'>커리어 성장</Link></li>
             <li className='menu-li mobile'>직군별 연봉</li>
--- src/components/GNBComponents/TitleComponent.tsx
+++ src/components/GNBComponents/TitleComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import '../style/TitleComponent.css'
 
@@ -8,7 +8,10 @@ function TitleComponent() {
             <div className='title-item desktop'>
                 <Link className='title-link' to='/'>Wanthing</Link>
             </div>
-            {/* TODO :: 홈에 마우스 올릴 때 파란색 밑줄 뜨도록 */}
+            {/* TODO :: 
+                    AS-IS :: 마우스 올릴 때 파란 밑줄
+                    TO-BE :: 해당 페이지에 있을 때 활성화
+              */}
             <div className='title-item mobile'>
                 <Link className='title-link' to='/'>홈</Link>
             </div>

1267ee20944ad79cb8674f078951070c239bf08b
201411108 <aggsae@gmail.com>
2021-11-03 22:06:27 +0900

    feat: add router Link in gnb components
    1. routing 경로 이동을 위한 Link 태그 작성
        * TitleComponent : / 이동
        * MenuComponent : /search 및 /career 이동

--- src/components/GNBComponents/MenuComponent.tsx
+++ src/components/GNBComponents/MenuComponent.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../style/MenuComponent.css'
 
 function MenuComponent() {
     return (
         <ul className='menu-ul'>
-            <li className='menu-li desktop'>탐색</li>
-            <li className='menu-li desktop'>커리어 성장</li>
+            {/* TODO :: 탐색 및 커리어 성장에 마우스 올렸을 때 파란색 밑줄이 뜨도록 */}
+            <li className='menu-li desktop'><Link className='menu-link' to='/search'>탐색</Link></li>
+            <li className='menu-li desktop'><Link className='menu-link' to='/career'>커리어 성장</Link></li>
             <li className='menu-li mobile'>직군별 연봉</li>
             <li className='menu-li mobile'>이력서</li>
             <li className='menu-li mobile'>매치업</li>
--- src/components/GNBComponents/TitleComponent.tsx
+++ src/components/GNBComponents/TitleComponent.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../style/TitleComponent.css'
 
 function TitleComponent() {
     return (
         <>
-            <div className='title-item desktop'>Wanthing</div>
-            <div className='title-item mobile'>홈</div>
+            <div className='title-item desktop'>
+                <Link className='title-link' to='/'>Wanthing</Link>
+            </div>
+            {/* TODO :: 홈에 마우스 올릴 때 파란색 밑줄 뜨도록 */}
+            <div className='title-item mobile'>
+                <Link className='title-link' to='/'>홈</Link>
+            </div>
         </>
     )
 }

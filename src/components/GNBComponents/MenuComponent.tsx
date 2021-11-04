1092b17d7c17ed5c81ae522f68423a7a048676c0
201411108 <aggsae@gmail.com>
2021-11-04 23:06:06 +0900

    style: add interactivity in title and menu
    1. 오직 모바일에서만 홈에 마우스 올렸을 때 밑줄 표시
    2. menu 컴포넌트의 클래스 이름 수정
       * BEFORE :: 데스크톱에서 보이는 요소들의 클래스 이름이 mobile로 되어
       있음
       * AFTER :: 데스크톱에서 보이는 요소는 desktop, 둘 다 보이는 요소는
       both로 수정
    3. menu 컴포넌트에서 데스크톱에서는 회색 밑줄, 모바일에서는 파란색
    밑줄이 표시되도록 작성

--- src/components/GNBComponents/MenuComponent.tsx
+++ src/components/GNBComponents/MenuComponent.tsx
@@ -10,13 +10,13 @@ function MenuComponent() {
                     TO-BE :: 해당 페이지에 있을 때 활성화, 데스크톱에서는 회색 밑줄
                              desktop, mobile이 반대로 되어 있음. 각 보이는 화면에 대해서(mobile -> mobile에서만 보이는 요소라는 뜻)
               */}
-            <li className='menu-li desktop'><Link className='menu-link' to='/search'>탐색</Link></li>
-            <li className='menu-li desktop'><Link className='menu-link' to='/career'>커리어 성장</Link></li>
-            <li className='menu-li mobile'>직군별 연봉</li>
-            <li className='menu-li mobile'>이력서</li>
-            <li className='menu-li mobile'>매치업</li>
-            <li className='menu-li mobile'>프리랜서</li>
-            <li className='menu-li mobile'>AI 합격예측</li>
+            <li className='menu-li both'><Link className='menu-link' to='/search'>탐색</Link></li>
+            <li className='menu-li both'><Link className='menu-link' to='/career'>커리어 성장</Link></li>
+            <li className='menu-li desktop'>직군별 연봉</li>
+            <li className='menu-li desktop'>이력서</li>
+            <li className='menu-li desktop'>매치업</li>
+            <li className='menu-li desktop'>프리랜서</li>
+            <li className='menu-li desktop'>AI 합격예측</li>
         </ul>
     )
 }
